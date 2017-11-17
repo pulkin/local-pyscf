d12382489d5afb2204c2568f07c687e442b16b5d
Unknown <gpulkin@gmail.com>
2017-11-17 15:56:16 +0900

    Refactored integral provider, added distribution functions, improvements

--- common.py
+++ common.py
@@ -1,6 +1,8 @@
+import itertools
+
 import numpy
-from scipy import special
 from pyscf import gto
+from scipy import special
 
 
 def transform(o, psi, axes="all", mode="fast"):
@@ -53,10 +55,8 @@ class AbstractIntegralProvider(object):
         Args:
             mol (pyscf.gto.mole.Mole): the Mole object;
         """
-        self.mol = mol
-
-    def __ao_labels_at__(self):
-        return numpy.array(tuple(i[0] for i in self.mol.ao_labels(fmt=False)))
+        self.__mol__ = mol
+        self.__ao_ownership__ = numpy.array(tuple(i[0] for i in self.__mol__.ao_labels(fmt=False)))
 
     def get_atom_basis(self, atoms, domain=None):
         """
@@ -68,7 +68,8 @@ class AbstractIntegralProvider(object):
         Returns:
             A list of basis functions' indices.
         """
-        ao = self.__ao_labels_at__()
+        ao = self.__ao_ownership__
+        atoms = self.__dressed_atoms__(atoms)
         if domain is not None:
             mask = (ao[:, numpy.newaxis] == numpy.array(domain)[numpy.newaxis, :]).sum(axis=1)
             ao = ao[numpy.argwhere(mask)]
@@ -92,7 +93,7 @@ class AbstractIntegralProvider(object):
 
     def __dressed_atoms__(self, atoms):
         if atoms is None:
-            return tuple(range(self.mol.natm))
+            return tuple(range(self.__mol__.natm))
         elif isinstance(atoms, int):
             return atoms,
         else:
@@ -108,9 +109,9 @@ class AbstractIntegralProvider(object):
             A list of tuples with ranges of shell slices.
         """
         atoms = self.__dressed_atoms__(atoms)
-        result = numpy.zeros(self.mol._bas.shape[0]+2, dtype=bool)
+        result = numpy.zeros(self.__mol__._bas.shape[0] + 2, dtype=bool)
         for a in atoms:
-            result[1:-1] = numpy.logical_or(result[1:-1], self.mol._bas[:, gto.ATOM_OF] == a)
+            result[1:-1] = numpy.logical_or(result[1:-1], self.__mol__._bas[:, gto.ATOM_OF] == a)
         r1 = result[:-1]
         r2 = result[1:]
         fr = numpy.argwhere(numpy.logical_and(numpy.logical_not(r1), r2))[:, 0]
@@ -126,43 +127,7 @@ class AbstractIntegralProvider(object):
         Returns:
             The total number of basis functions.
         """
-        return sum(self.mol.bas_len_cart(i) for i in self.mol.atom_shell_ids(atom))
-
-
-class IntegralProvider(AbstractIntegralProvider):
-    def intor(self, name, *shells, **kwargs):
-        """
-        A version of `pyscf.mole.Mole.intor` accepting lists of shell ranges instead of single ranges.
-        Args:
-            name (str): integral name;
-            *shells (nested list): shell ranges lists;
-            **kwargs: keywords passed to `pyscf.mole.Mole.intor`;
-
-        Returns:
-            An array with integrals.
-        """
-        shls_slice = tuple()
-        basis_size = []
-        for dim, shell_list in enumerate(shells):
-            shell_list = numpy.array(shell_list)
-            if len(shell_list.shape) == 1:
-                shls_slice += tuple(shell_list)
-            elif len(shell_list.shape) != 2:
-                raise ValueError("Cannot recognize shell list: {}".format(repr(shell_list)))
-            elif len(shell_list) == 1:
-                shls_slice += tuple(shell_list[0])
-            else:
-                ints = []
-                for sh in shell_list:
-                    ints.append(self.intor(name, *list(shells[:dim]+(sh,)+shells[dim+1:]), **kwargs))
-                return numpy.concatenate(ints, axis=dim)
-            basis_size.append(sum(
-                self.mol.bas_len_cart(i) for i in range(shls_slice[-2], shls_slice[-1])
-            ))
-        kwargs["shls_slice"] = shls_slice
-        result = self.mol.intor(name, **kwargs).view()
-        result.shape = tuple(basis_size)
-        return result
+        return sum(self.__mol__.bas_len_cart(i) for i in self.__mol__.atom_shell_ids(atom))
 
     def intor_atoms(self, name, *atoms, **kwargs):
         """
@@ -175,7 +140,7 @@ class IntegralProvider(AbstractIntegralProvider):
         Returns:
             An array or tensor with integrals.
         """
-        return self.intor(name, *(self.shell_ranges(i) for i in atoms), **kwargs)
+        raise NotImplementedError
 
     def get_ovlp(self, atoms1, atoms2):
         """
@@ -238,6 +203,131 @@ class IntegralProvider(AbstractIntegralProvider):
             A four-index tensor with ERIs belonging to a given subset of atoms.
         """
         return self.intor_atoms("int2e_sph", atoms1, atoms2, atoms3, atoms4)
+
+
+def intor(mol, name, *shells, **kwargs):
+    """
+    A version of `pyscf.mole.Mole.intor` accepting lists of shell ranges instead of single ranges.
+    Args:
+        mol (pyscf.mol.Mole): the Mole object;
+        name (str): integral name;
+        *shells (nested list): shell ranges lists;
+        **kwargs: keywords passed to `pyscf.mole.Mole.intor`;
+
+    Returns:
+        An array with integrals.
+    """
+    shls_slice = tuple()
+    basis_size = []
+    for dim, shell_list in enumerate(shells):
+        shell_list = numpy.array(shell_list)
+        if len(shell_list.shape) == 1:
+            shls_slice += tuple(shell_list)
+        elif len(shell_list.shape) != 2:
+            raise ValueError("Cannot recognize shell list: {}".format(repr(shell_list)))
+        elif len(shell_list) == 1:
+            shls_slice += tuple(shell_list[0])
+        else:
+            ints = []
+            for sh in shell_list:
+                ints.append(intor(mol, name, *list(shells[:dim]+(sh,)+shells[dim+1:]), **kwargs))
+            return numpy.concatenate(ints, axis=dim)
+        basis_size.append(sum(
+            mol.bas_len_cart(i) for i in range(shls_slice[-2], shls_slice[-1])
+        ))
+    kwargs["shls_slice"] = shls_slice
+    result = mol.intor(name, **kwargs).view()
+    result.shape = tuple(basis_size)
+    return result
+
+
+class IntegralProvider(AbstractIntegralProvider):
+    def intor_atoms(self, name, *atoms, **kwargs):
+        return intor(self.__mol__, name, *(self.shell_ranges(i) for i in atoms), **kwargs)
+
+    intor_atoms.__doc__ = AbstractIntegralProvider.intor_atoms.__doc__
+
+
+def as_dict(tensor, provider):
+    """
+    Converts a tensor into a dict form where keys are atomic indexes.
+    Args:
+        tensor (numpy.ndarray): a tensor to convert;
+        provider (IntegralProvider): provider of integrals;
+
+    Returns:
+        A dict with tensor blocks.
+    """
+    result = {}
+    r = range(len(provider.__mol__.natm))
+    for a in itertools.product((r,)*len(tensor.shape)):
+        result[a] = tensor[provider.get_block(*a)]
+    return result
+
+
+def pairs(n, s1=0, s2=0, symmetry=True):
+    """
+    Iterates over atom pairs.
+
+    Args:
+        n (int): total number of atoms;
+        s1 (int): starting point - atom 1 index;
+        s2 (int): starting point - atom 2 index;
+        symmetry (bool): whether symmetries are taken into acocunt;
+
+    Yields:
+        Pairs of atomic indexes.
+    """
+    for i in range(s1*n+s2, n**2):
+        a1 = i // n
+        a2 = i % n
+        if not symmetry or (a1 <= a2):
+            yield a1, a2
+
+
+def get_sparse_eri(provider, threshold=1e-12):
+    """
+    Retrieves a sparse representation of electron repulsion integrals.
+    Args:
+        provider (IntegralProvider): integral provider;
+        threshold (float): a threshold for the Cauchy-Schwartz estimation speciifying integral blocks to discard;
+
+    Returns:
+        A dict where keys are four atomic indexes and values are four-center integral blocks.
+    """
+
+    diagonal = {}
+
+    for a1, a2 in pairs(provider.__mol__.natm):
+        integrals = provider.get_eri(a1, a2, a1, a2)
+        diagonal[a1, a2] = integrals
+
+    eri = {}
+    for a1, a2 in pairs(provider.__mol__.natm):
+        for a3, a4 in pairs(provider.__mol__.natm, a1, a2):
+            if (diagonal[a1, a2] * diagonal[a3, a4]).max() > threshold ** 2:
+                if a1 == a3 and a2 == a4:
+                    integrals = diagonal[a1, a2]
+
+                    eri[a1, a2, a3, a4] = integrals
+                    eri[a2, a1, a3, a4] = integrals.transpose((1, 0, 2, 3))
+                    eri[a1, a2, a4, a3] = integrals.transpose((0, 1, 3, 2))
+                    eri[a2, a1, a4, a3] = integrals.transpose((1, 0, 3, 2))
+
+                else:
+                    integrals = provider.get_eri(a1, a2, a3, a4)
+
+                    eri[a1, a2, a3, a4] = integrals
+                    eri[a2, a1, a3, a4] = integrals.transpose((1, 0, 2, 3))
+                    eri[a1, a2, a4, a3] = integrals.transpose((0, 1, 3, 2))
+                    eri[a2, a1, a4, a3] = integrals.transpose((1, 0, 3, 2))
+
+                    eri[a3, a4, a1, a2] = integrals.transpose((2, 3, 0, 1))
+                    eri[a4, a3, a1, a2] = integrals.transpose((3, 2, 0, 1))
+                    eri[a3, a4, a2, a1] = integrals.transpose((2, 3, 1, 0))
+                    eri[a4, a3, a2, a1] = integrals.transpose((3, 2, 1, 0))
+
+    return eri
 
 
 class SimpleCachingIntegralProvider(IntegralProvider):
