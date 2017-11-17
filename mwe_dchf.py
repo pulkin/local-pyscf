fc26420bed94aecd86e01a285434206582a6e060
Unknown <gpulkin@gmail.com>
2017-11-17 16:02:51 +0900

    Added CC to DC MWE

--- mwe_dchf.py
+++ mwe_dchf.py
@@ -7,3 +7,7 @@ model = hydrogen_dimer_chain(24)
 mf = dchf.DCHF(model)
 assign_domains(mf, 4, 2)
 mf.kernel()
+
+print "Custom MP2", dchf.energy_2(mf.domains, 1.0, with_t2=False)[0]
+print "Pyscf MP2", dchf.energy_2(mf.domains, 1.0, amplitude_calculator=dchf.pyscf_mp2_amplitude_calculator, with_t2=False)[0]
+print "Pyscf CCSD", dchf.energy_2(mf.domains, 1.0, amplitude_calculator=dchf.pyscf_ccsd_amplitude_calculator, with_t2=False)[0]
