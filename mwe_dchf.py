1eab9ca8069d0c1f784355aa9289185ce11c872a
Unknown <gpulkin@gmail.com>
2017-12-01 18:09:37 -0800

    Random changes in DCHF MWE

--- mwe_dchf.py
+++ mwe_dchf.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 from test_common import hydrogen_dimer_chain
-from test_dchf import assign_domains
+from test_dchf import assign_chain_domains
 import dchf
+from pyscf.scf import RHF
 
 model = hydrogen_dimer_chain(24)
-mf = dchf.DCHF(model)
-assign_domains(mf, 4, 2)
+model.verbose = 4
+
+mf = RHF(model)
 mf.kernel()
 
-print "Custom MP2", dchf.energy_2(mf.domains, 1.0, with_t2=False)[0]
-print "Pyscf MP2", dchf.energy_2(mf.domains, 1.0, amplitude_calculator=dchf.pyscf_mp2_amplitude_calculator, with_t2=False)[0]
-print "Pyscf CCSD", dchf.energy_2(mf.domains, 1.0, amplitude_calculator=dchf.pyscf_ccsd_amplitude_calculator, with_t2=False)[0]
+mf = dchf.DCHF(model)
+assign_chain_domains(mf, 2, 2)
+mf.kernel()
+
+# print "Custom MP2", dchf.energy_2(mf.domains, 1.0, with_t2=False)[0]
+# print "Pyscf MP2", dchf.energy_2(mf.domains, 1.0, amplitude_calculator=dchf.pyscf_mp2_amplitude_calculator, with_t2=False)[0]
+# print "Pyscf CCSD", dchf.energy_2(mf.domains, 1.0, amplitude_calculator=dchf.pyscf_ccsd_amplitude_calculator, with_t2=False)[0]
