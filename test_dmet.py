4f411a4ffab86c522f43ce23bbfde1fb192237f2
Unknown <gpulkin@gmail.com>
2018-01-15 15:44:45 -0800

    Added FCI test to DMET and small changes in notation

--- test_dmet.py
+++ test_dmet.py
@@ -13,7 +13,7 @@ class HubbardModelTest(unittest.TestCase):
     def setUpClass(cls):
         cls.n = 12
         cls.u = 8.0
-        cls.nelec = 6
+        cls.nelec = 10
         cls.mf = hubbard_model_driver(cls.u, cls.n, cls.nelec)
 
     def __test_dummy__(self, sc, fragment_size, **kwargs):
@@ -35,7 +35,7 @@ class HubbardModelTest(unittest.TestCase):
         )
         dummy_dmet.kernel()
 
-        testing.assert_allclose(e_ref, dummy_dmet.e_tot)
+        testing.assert_allclose(e_ref, dummy_dmet.e_tot, rtol=1e-6)
         if dummy_dmet.conv_tol is not None:
             testing.assert_allclose([0], dummy_dmet.convergence_history)
 
@@ -96,7 +96,7 @@ class HubbardModelTest(unittest.TestCase):
             style="interacting-bath",
         )
 
-    def test_non_self_consistent(self):
+    def test_dummy_non_self_consistent(self):
         """
         Test a dummy interacting-bath non-self-consistent setup where the fragment solver is RHF. The fragment size is
         1.
@@ -109,3 +109,45 @@ class HubbardModelTest(unittest.TestCase):
             conv_tol=None,
         )
 
+    def test_fci_mu(self):
+        """
+        Test whether chemical potential-based self-consistency converges immediately with FCI solver.
+        """
+        self.mf.kernel()
+        e_ref = self.mf.e_tot
+
+        fci_dmet = dmet.DMET(
+            self.mf,
+            common.ModelFCI,
+            dmet.MuFragmentDMETUSC,
+            numpy.arange(self.n).reshape(-1, 2),
+            associate="all",
+            style="interacting-bath",
+        )
+        fci_dmet.kernel()
+
+        # A single iteration is required to fit the non-zero chemical potential of the mean-field solver.
+        # The second iteration should converge to zero
+        testing.assert_equal(2, len(fci_dmet.convergence_history))
+        testing.assert_array_less(0, e_ref)
+        testing.assert_array_less(fci_dmet.e_tot, 0)
+
+    def test_fci_1(self):
+        """
+        Performs an FCI test with a fragment size equal to 1.
+        """
+        self.mf.kernel()
+        e_ref = self.mf.e_tot
+        testing.assert_array_less(0, e_ref)
+
+        fci_dmet = dmet.DMET(
+            self.mf,
+            common.ModelFCI,
+            dmet.FragmentFullDMETUSC,
+            numpy.arange(self.n).reshape(-1, 2),
+            associate="all",
+            style="interacting-bath",
+        )
+        fci_dmet.kernel()
+        testing.assert_array_less(fci_dmet.e_tot, 0)
+        testing.assert_array_less(2, len(fci_dmet.convergence_history))
