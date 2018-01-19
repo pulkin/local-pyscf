4f65380e944bb578f7177d5db1418ebe0471fb6f
Unknown <gpulkin@gmail.com>
2018-01-19 01:47:14 -0800

    Added ab-initio Hydrogen chain tests

--- test_dmet.py
+++ test_dmet.py
@@ -151,3 +151,86 @@ class HubbardModelTest(unittest.TestCase):
         fci_dmet.kernel()
         testing.assert_array_less(fci_dmet.e_tot, 0)
         testing.assert_array_less(2, len(fci_dmet.convergence_history))
+
+
+class HydrogenChainTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mf = RHF(hydrogen_dimer_chain(10))
+
+    def __test_dummy__(self, sc, fragment_size, **kwargs):
+        """
+        A generic routine for self-consistency tests where the local fragment is treated with RHF.
+        Args:
+            sc: the self-consistency class;
+            fragment_size: the size of the fragment;
+            **kwargs: keyword arguments to DMET constructor;
+        """
+        self.mf.kernel()
+        e_ref = self.mf.e_tot
+        dummy_dmet = dmet.AbInitioDMET(
+            self.mf,
+            common.ModelRHF,
+            sc,
+            numpy.arange(self.mf.mol.natm).reshape(-1, fragment_size),
+            **kwargs
+        )
+        dummy_dmet.kernel(maxiter=2)
+
+        testing.assert_allclose(e_ref, dummy_dmet.e_tot, rtol=1e-6)
+        if dummy_dmet.conv_tol is not None:
+            testing.assert_allclose([0], dummy_dmet.convergence_history)
+
+    def test_dummy_interacting(self):
+        """
+        Test a dummy interacting-bath setup where the fragment solver is RHF. The fragment size is 1.
+        """
+        self.__test_dummy__(
+            dmet.FragmentFullDMETUSC,
+            1,
+            style="interacting-bath",
+        )
+
+    def test_dummy_2(self):
+        """
+        Test a dummy interacting-bath setup where the fragment solver is RHF. The fragment size is 2.
+        """
+        self.__test_dummy__(
+            dmet.FragmentFullDMETUSC,
+            2,
+            style="interacting-bath",
+        )
+
+    def test_dummy_local(self):
+        """
+        Test a dummy interacting-bath setup where the fragment solver is RHF and only the fragment part of the density
+        matrix is fitted. The fragment size is 1.
+        """
+        self.__test_dummy__(
+            dmet.FragmentFragmentDMETUSC,
+            1,
+            style="interacting-bath",
+        )
+
+    def test_dummy_mu(self):
+        """
+        Test a dummy interacting-bath setup where the fragment solver is RHF and only the potential is fitted. The
+        fragment size is 1.
+        """
+        self.__test_dummy__(
+            dmet.MuFragmentDMETUSC,
+            1,
+            style="interacting-bath",
+        )
+
+    def test_dummy_non_self_consistent(self):
+        """
+        Test a dummy interacting-bath non-self-consistent setup where the fragment solver is RHF. The fragment size is
+        1.
+        """
+        self.__test_dummy__(
+            dmet.FragmentFragmentDMETUSC,
+            1,
+            style="interacting-bath",
+            conv_tol=None,
+        )
